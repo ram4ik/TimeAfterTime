5fb83c9bf83fe1b1338311cb14748198a3422211
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-05-15 18:44:32 +0300

    Initial Commit

---
+++ TimeAfterTime/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TimeAfterTime
+//
+//  Created by Ramill Ibragimov on 15.05.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
